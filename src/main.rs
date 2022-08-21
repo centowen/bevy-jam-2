30f15ad2de911cb1556e5f42da90d6963dd72b8b
Magnus Sandén <thefreid@gmail.com>
2022-08-21 21:09:35 +0200

    Create a window with an inspector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::WorldInspectorPlugin;
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(Camera2dBundle::default());
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(WorldInspectorPlugin::new())
+        .insert_resource(ClearColor(Color::rgb(0.8, 0.85, 0.85)))
+        .add_startup_system(setup)
+        .run();
 }
