4a25f79b1077749c64605aa419326f3206d0e8bf
Magnus Sandén <thefreid@gmail.com>
2022-08-21 21:48:20 +0200

    Spawn crab sprites

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,42 @@ struct CrabSpawnTimer {
     timer: Timer,
 }
 
-fn setup(mut commands: Commands) {
+struct ImageAssets {
+    crab: Handle<Image>,
+}
+
+#[derive(Component)]
+struct Crab;
+
+fn setup(mut commands: Commands, server: Res<AssetServer>) {
     commands.spawn_bundle(Camera2dBundle::default());
     commands.insert_resource(CrabSpawnTimer {
         timer: Timer::new(Duration::from_secs(3), true),
     });
+    commands.insert_resource(ImageAssets {
+        crab: server.load("rustacean-flat-noshadow.png"),
+    });
 }
 
-fn spawn_crab(mut commands: Commands, time: Res<Time>, mut crab_timer: ResMut<CrabSpawnTimer>) {
+fn spawn_crab(
+    mut commands: Commands,
+    images: Res<ImageAssets>,
+    time: Res<Time>,
+    mut crab_timer: ResMut<CrabSpawnTimer>,
+) {
     crab_timer.timer.tick(time.delta());
 
     if crab_timer.timer.just_finished() {
-        eprintln!("🦀");
-        // commands.spawn_bundle(CrabBundle::new())
+        commands
+            .spawn_bundle(SpriteBundle {
+                texture: images.crab.clone(),
+                transform: Transform {
+                    scale: Vec3::splat(0.2),
+                    ..default()
+                },
+                ..default()
+            })
+            .insert(Crab);
     }
 }
 
