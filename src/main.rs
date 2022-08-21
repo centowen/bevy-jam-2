0cedf7480caaad3be3e234efd891b10df9712aa1
Magnus Sandén <thefreid@gmail.com>
2022-08-21 22:20:13 +0200

    Brownian crabs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_inspector_egui::WorldInspectorPlugin;
+use bevy_turborand::*;
 use std::time::Duration;
 
 struct CrabSpawnTimer {
@@ -13,10 +14,13 @@ struct ImageAssets {
 #[derive(Component)]
 struct Crab;
 
+#[derive(Component, Default)]
+struct Velocity(Vec2);
+
 fn setup(mut commands: Commands, server: Res<AssetServer>) {
     commands.spawn_bundle(Camera2dBundle::default());
     commands.insert_resource(CrabSpawnTimer {
-        timer: Timer::new(Duration::from_secs(3), true),
+        timer: Timer::new(Duration::from_millis(500), true),
     });
     commands.insert_resource(ImageAssets {
         crab: server.load("rustacean-flat-noshadow.png"),
@@ -36,12 +40,24 @@ fn spawn_crab(
             .spawn_bundle(SpriteBundle {
                 texture: images.crab.clone(),
                 transform: Transform {
-                    scale: Vec3::splat(0.2),
+                    scale: Vec3::splat(0.02),
                     ..default()
                 },
                 ..default()
             })
+            .insert(Velocity::default())
             .insert(Crab);
+    }
+}
+
+fn move_crabs(
+    mut q_crabs: Query<(&mut Transform, &mut Velocity), With<Crab>>,
+    mut rng: ResMut<GlobalRng>,
+) {
+    for (mut transform, mut velocity) in q_crabs.iter_mut() {
+        velocity.0 += Vec2::new(rng.f32() * 2.0 - 1.0, rng.f32() * 2.0 - 1.0);
+        velocity.0 *= 0.8;
+        transform.translation += velocity.0.extend(0.0);
     }
 }
 
@@ -49,8 +65,10 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(RngPlugin::default())
         .insert_resource(ClearColor(Color::rgb(0.8, 0.85, 0.85)))
         .add_startup_system(setup)
         .add_system(spawn_crab)
+        .add_system(move_crabs)
         .run();
 }
