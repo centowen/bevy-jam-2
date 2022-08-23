5325dc5800d6d4261ece59b9cadcc237190be189
Hampus Linander <hampus.linander@gmail.com>
2022-08-23 11:35:06 +0200

    Persistent direction crabs

--- src/main.rs
+++ src/main.rs
@@ -56,8 +56,15 @@ fn move_crabs(
     mut rng: ResMut<GlobalRng>,
 ) {
     for (mut transform, mut velocity, mut sprite) in q_crabs.iter_mut() {
-        velocity.0 += Vec2::new(rng.f32() * 2.0 - 1.0, rng.f32() * 2.0 - 1.0);
-        velocity.0 *= 0.8;
+        let mut v_norm = (velocity.0.x.powf(2.0) + velocity.0.y.powf(2.0)).powf(0.5);
+        if v_norm < 0.0001 {
+            velocity.0 = Vec2::new(rng.f32() * 2.0 - 1.0, rng.f32() * 2.0 - 1.0);
+            v_norm = (velocity.0.x.powf(2.0) + velocity.0.y.powf(2.0)).powf(0.5);
+        }
+        let nv = velocity.0 / v_norm;
+        let perp_nv = Vec2::new(nv.y, -nv.x);
+        velocity.0 += (rng.f32() * 0.2 - 0.1) * perp_nv;
+        velocity.0 += (rng.f32() * 0.1 - 0.05) * nv;
         transform.translation += velocity.0.extend(0.0);
         let angle = (PI / 2.0) - f32::atan2(velocity.0.x, velocity.0.y);
         transform.rotation = Quat::from_rotation_z(angle);
